01bd6c26c9f56e7f004015e84c2c0258cff43b78
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-25 16:44:36 +0200

    Configure build path

---
+++ src/main/java/src/main/java/MainClass.java
@@ -0,0 +1,15 @@
+package src.main.java;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Commit 1");
+		
+		System.out.println("Commit 2");
+		
+		System.out.println("Commit 3");
+
+	}
+
+}
