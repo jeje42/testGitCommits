620f61a69e62a4f7bab7f5ae5ccdc9d940982679
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-26 09:55:36 +0200

    Commit 1

--- src/main/java/src/main/java/MainClass.java
+++ src/main/java/src/main/java/MainClass.java
@@ -5,12 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Commit 1");
-		
-		System.out.println("Commit 2");
-		
-		System.out.println("Commit 3");
-		
-		testRebase tR = new testRebase();
 
 	}
 
