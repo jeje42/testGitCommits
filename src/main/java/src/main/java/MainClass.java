4bb4c413e2dfede6156c2f48688d647e49d37d48
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-26 09:56:03 +0200

    Commit 2 Commit 3

--- src/main/java/src/main/java/MainClass.java
+++ src/main/java/src/main/java/MainClass.java
@@ -5,6 +5,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Commit 1");
+		
+		System.out.println("Commit 2");
+		
+		System.out.println("Commit 3");
 
 	}
 
